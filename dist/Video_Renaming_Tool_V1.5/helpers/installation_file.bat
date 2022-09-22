ca1d018264e8d1279b8d91efa3379db6396c4366
TimArthur391 <arthurt1995@googlemail.com>
2022-09-22 13:20:25 +0100

    Code made more tidy, and commentted accordingly

---
+++ dist/Video_Renaming_Tool_V1.5/helpers/installation_file.bat
@@ -0,0 +1,20 @@
+rem This file will allow the installation to be done through a single click in the Code_Repository
+rem by the user. It will also be used to allow automatic updates to made. Follow the intructions
+rem to edit this file accordingly
+
+rem Be sure to update the name of the application and version number
+echo RUNNING INSTALLATION OF 'Gait_Lab_Video_Renaming_Tool' VERSON '1.4'
+
+rem this copies the installation file to the users documents. replace 'A:\ZIP_FILE_LOCATION' with
+rem the file path of the application bundle or file.
+xcopy /s A:\Code_repository\Matlab_Python_Code\Validated\25_Gait_Lab_Video_Renaming_Tool\Video_Renaming_Tool_V1.4.zip %userprofile%\Documents
+
+rem this will extract the contents of the zip'd file into the documents folder. Be sure to change
+rem the 'FILENAME.zip' in the line bellow with your zip'd file name
+powershell -command "Expand-Archive -Force '%userprofile%\Documents\Video_Renaming_Tool_V1.4.zip' '%userprofile%\Documents'"
+
+rem this will create a shortcut to the application you wish to run.
+rem edit 'APPLICATION_shortcut.lnk' below to the name of the shortcut you want to create
+rem edit 'Documents\FILENAME\APPLICATION_NAME.exe' below to the location of your application exectuable
+rem edit 'Documents\FILENAME' below to the folder location of your application executable
+powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Desktop\Video_Renaming_Tool_V1.4_shortcut.lnk');$s.TargetPath='%userprofile%\Documents\Video_Renaming_Tool_V1.4\Video_Renaming_Tool_V1.4.exe';$s.WorkingDirectory='%userprofile%\Documents\Video_Renaming_Tool_V1.4';$s.Save()"
